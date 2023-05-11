543abbdbedd0c5e14ae7f7699dfb86d0995ced06
Agustín Ruatta <agustinruatta@gmail.com>
2023-05-10 21:48:50 -0300

    Obtener datos de producto

--- script.js
+++ script.js
@@ -20,3 +20,41 @@ function agregarComentario(evento) {
 
 document.getElementById('boton-enviar-comentario')
     .addEventListener('click', agregarComentario)
+
+async function cargarDatosProducto() {
+    let response = await fetch('https://my-json-server.typicode.com/agustinruatta/fake_json_server_db/products/1');
+    let producto = await response.json();
+
+    let titulo = document.createTextNode(producto.title);
+    document.getElementById('titulo-producto').appendChild(titulo);
+
+    let descripcion = document.createTextNode(producto.description);
+    document.getElementById('texto-descripcion').appendChild(descripcion);
+
+    document.getElementById('imagen').src = producto.image_url;
+
+    document.getElementById('link-fabricante').href = producto.factory_url;
+
+    for (let tipoNotebook of producto.notebooksTypes) {
+        let subtituloTipoComputadora = document.createElement('h3');
+        subtituloTipoComputadora.appendChild(document.createTextNode('Tipo computadora:'));
+
+        let listaDesordenada = document.createElement('ul');
+        let precio = document.createElement('li');
+        let ram = document.createElement('li');
+
+        let precioTextNode = document.createTextNode('Precio: ' + tipoNotebook.price);
+        let ramTextoNode = document.createTextNode('RAM: ' + tipoNotebook.ramAmount);
+
+        precio.appendChild(precioTextNode);
+        ram.appendChild(ramTextoNode);
+
+        listaDesordenada.appendChild(precio);
+        listaDesordenada.appendChild(ram);
+
+        document.getElementById('descripcion').appendChild(subtituloTipoComputadora);
+        document.getElementById('descripcion').appendChild(listaDesordenada);
+    }
+}
+
+cargarDatosProducto();
