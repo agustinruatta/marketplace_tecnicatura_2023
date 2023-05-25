759c65fa830c2ec9526813437f83610fb99b3745
Agustín Ruatta <agustinruatta@gmail.com>
2023-05-24 21:44:10 -0300

    Agregado eleccion de producto

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const app = Vue.createApp({
             "description": "Computadora HP orientado para gama media. Procesador Intel® Core™ i3 de 11.ª generación.  Windows 10 Home 64.  Unidad de estado sólido PCIe® NVMe™ M.2 de 256 GB . Pantalla de 14 pulgadas.",
             "image_url": "https://ar-media.hptiendaenlinea.com/catalog/product/8/V/8VW01LA-1_T1615590539.png",
             "factory_url": "https://www.hp.com/ar-es/shop/notebook-hp-14-dq2024la-3v8j6la.html",
+            notebookSeleccionada: 0,
             "notebooksTypes": [
                 {
                     "ramAmount": "8 GB",
@@ -31,6 +32,9 @@ const app = Vue.createApp({
         },
         cambiarImagen(imagenUrl) {
             this.image_url = imagenUrl;
+        },
+        seleccionarTipoComputadora(indice) {
+            this.notebookSeleccionada = indice;
         }
     }
 })
