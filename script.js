4d7c00a7e714ac6f4c730e701904f3e25a434a9c
Agustín Ruatta <agustinruatta@gmail.com>
2023-05-24 22:00:19 -0300

    Agregado carro de compra

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ const app = Vue.createApp({
     data() {
         return {
             "id": 1,
-            cantidadComprados: 0,
             "title": "Notebook HP 14-dq2024la",
             "description": "Computadora HP orientado para gama media. Procesador Intel® Core™ i3 de 11.ª generación.  Windows 10 Home 64.  Unidad de estado sólido PCIe® NVMe™ M.2 de 256 GB . Pantalla de 14 pulgadas.",
             "image_url": "https://ar-media.hptiendaenlinea.com/catalog/product/8/V/8VW01LA-1_T1615590539.png",
             "factory_url": "https://www.hp.com/ar-es/shop/notebook-hp-14-dq2024la-3v8j6la.html",
             notebookSeleccionada: 0,
+            notebooksCompradas: [],
             "notebooksTypes": [
                 {
-                    "ramAmount": "8 GB",
-                    "price": 100000
+                    ramAmount: "8 GB",
+                    price: 100000
                 },
                 {
-                    "ramAmount": "16 GB",
-                    "price": 122547
+                    ramAmount: "16 GB",
+                    price: 122547
                 }
             ],
             imagenes: [
@@ -27,8 +27,8 @@ const app = Vue.createApp({
         }
     },
     methods: {
-        clickBotonCompra() {
-            this.cantidadComprados += 1;
+        clickBotonCompra(indice) {
+            this.notebooksCompradas.push(this.notebooksTypes[indice]);
         },
         cambiarImagen(imagenUrl) {
             this.image_url = imagenUrl;
@@ -36,6 +36,16 @@ const app = Vue.createApp({
         seleccionarTipoComputadora(indice) {
             this.notebookSeleccionada = indice;
         }
+    },
+    computed: {
+        total() {
+            let total = 0;
+            for(let notebookComprada of this.notebooksCompradas) {
+                total += notebookComprada.price;
+            }
+
+            return total;
+        },
     }
 })
 
