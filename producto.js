b2278ba5cebc586ceb786908f0f00a20e4d201ff
Agustín Ruatta <agustinruatta@gmail.com>
2023-05-31 21:57:09 -0300

    Movido a componentes

--- producto.js
+++ producto.js
@@ -3,12 +3,7 @@ const app = Vue.createApp({
         return {
             "id": 1,
             "title": "Notebook HP 14-dq2024la",
-            "description": "Computadora HP orientado para gama media. Procesador Intel® Core™ i3 de 11.ª generación.  Windows 10 Home 64.  Unidad de estado sólido PCIe® NVMe™ M.2 de 256 GB . Pantalla de 14 pulgadas.",
-            "image_url": "https://ar-media.hptiendaenlinea.com/catalog/product/8/V/8VW01LA-1_T1615590539.png",
-            "factory_url": "https://www.hp.com/ar-es/shop/notebook-hp-14-dq2024la-3v8j6la.html",
-            notebookSeleccionada: 0,
-            notebooksCompradas: [],
-            "notebooksTypes": [
+            notebooksTypes: [
                 {
                     ramAmount: "8 GB",
                     price: 100000
@@ -18,6 +13,10 @@ const app = Vue.createApp({
                     price: 122547
                 }
             ],
+            "image_url": "https://ar-media.hptiendaenlinea.com/catalog/product/8/V/8VW01LA-1_T1615590539.png",
+            "factory_url": "https://www.hp.com/ar-es/shop/notebook-hp-14-dq2024la-3v8j6la.html",
+            notebookSeleccionada: 0,
+            notebooksCompradas: [],
             imagenes: [
                 'https://ar-media.hptiendaenlinea.com/catalog/product/8/V/8VW01LA-1_T1615590539.png',
                 'https://mla-s1-p.mlstatic.com/873896-MLA48241212970_112021-F.jpg',
@@ -27,17 +26,18 @@ const app = Vue.createApp({
         }
     },
     methods: {
-        clickBotonCompra(indice) {
-            this.notebooksCompradas.push(this.notebooksTypes[indice]);
-        },
         cambiarImagen(imagenUrl) {
             this.image_url = imagenUrl;
         },
-        seleccionarTipoComputadora(indice) {
-            this.notebookSeleccionada = indice;
-        },
+
         eliminarCompra(indice) {
             this.notebooksCompradas = this.notebooksCompradas.filter((value, index) => index !== indice);
+        },
+        computadoraComprada(notebook) {
+            this.notebooksCompradas.push(notebook);
+        },
+        manejarNotebookSeleccionada(indice) {
+            this.notebookSeleccionada = indice;
         }
     },
     computed: {
@@ -51,5 +51,3 @@ const app = Vue.createApp({
         },
     }
 })
-
-app.mount('#grid-layout')
