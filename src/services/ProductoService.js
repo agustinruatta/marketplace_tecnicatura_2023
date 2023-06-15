8e63309ae51b6ae08ad00319aa48c1e1329047ae
Agustín Ruatta <agustinruatta@gmail.com>
2023-06-14 21:47:25 -0300

    Llamar a API en pagina principal

--- src/services/ProductoService.js
+++ src/services/ProductoService.js
@@ -4,5 +4,9 @@ export default {
     getProducto() {
         return axios
             .get('https://my-json-server.typicode.com/agustinruatta/fake_json_server_db/products/1')
+    },
+    getProductos() {
+        return axios
+            .get('https://my-json-server.typicode.com/agustinruatta/fake_json_server_db/products/')
     }
 }
--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -9,7 +9,10 @@
     <p>Cantidad de artículos: <strong id="cantidad-articulos"></strong></p>
 
     <div id="listado-productos">
-      <div class="preview-producto" v-for="(producto, index) in products" :key="index">
+      <div v-if="products.length === 0">
+        <h1>Cargando</h1>
+      </div>
+      <div v-else class="preview-producto" v-for="(producto, index) in products" :key="index">
         <router-link to="/producto">
           <img :src="producto.image_url">
           <strong>{{ producto.title }}</strong>
@@ -20,30 +23,25 @@
 </template>
 
 <script>
+import productoService from '@/services/ProductoService';
 
 export default {
   name: 'IndexView',
   data() {
     return {
-      products: [
-        {
-          "title": "Notebook HP 14-dq2024la",
-          "image_url": "https://ar-media.hptiendaenlinea.com/catalog/product/8/V/8VW01LA-1_T1615590539.png",
-        },
-        {
-          "title": "Notebook Max L5 i7 Microsoft 365 Personal ",
-          "image_url": "https://bangho.vtexassets.com/arquivos/ids/160155/Notebook-Max-L5-i5-cuotas-sin-interes.jpg?v=637697603076200000",
-        },
-        {
-          "title": "Notebook N14WCE128 14\" IntelCeleron N3350 128GB Noblex",
-          "image_url": "https://noblex.com.ar/media/catalog/product/cache/aa999612044d357928d16abd893bc3dd/1/_/1_5_1.jpg",
-        },
-        {
-          "title": "Notebook Dell 3190 Intel Celeron 4Gb SSD 128Gb 11,6\" HD Windows 10",
-          "image_url": "https://www.bidcom.com.ar/publicacionesML/productos/NOT00260/1000x1000-NOT00260.jpg",
-        }
-      ]
+      products: []
     }
+  },
+  async created() {
+    /*
+    productoService.getProductos()
+        .then(respuesta => {
+          this.products = respuesta.data;
+        })
+    */
+
+    let respuesta = await productoService.getProductos();
+    this.products = respuesta.data;
   }
 }
 </script>
